455e885a5fc003debfe751a1b5276eca2a6f015d
Ariel Santangelo <>
2026-02-24 10:33:33 -0300

    changed version from 25.11 to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS Gaming System";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
   outputs = { self, nixpkgs }:
